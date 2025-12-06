5bd20cfc9e3140457cd6740aa30b2f59dd9324ea
WolfyXypth <Wolfexypth@gmail.com>
2025-12-06 13:32:01 +0545

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,21 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // <--- CRITICAL: Map looks broken without this
 
-import { MapContainer, TileLayer } from "react-leaflet";
-
-function Map() {
+const Map = () => {
   return (
-    <MapContainer
-      center={[27.7172, 85.3240]}   // Kathmandu
-      zoom={20}
+    // The container needs an explicit height, or the map won't show up.
+    <MapContainer 
+      center={[27.7172, 85.3240]} 
+      zoom={20} 
       style={{ height: "100vh", width: "100%" }}
     >
       <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="© OpenStreetMap contributors"
       />
     </MapContainer>
   );
-}
+};
 
 export default Map;
