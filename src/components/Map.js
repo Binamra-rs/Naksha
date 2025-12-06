59bce07c9f3f071456beb928283a0b65308ab3ed
WolfyXypth <Wolfexypth@gmail.com>
2025-12-06 13:07:17 +0545

    updated map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,18 @@
-import React from 'react';
+
+import { MapContainer, TileLayer } from "react-leaflet";
 
 function Map() {
   return (
-    <section style={{ padding: '20px' }}>
-      <h2> Map</h2>
-      <p>interactive map view.</p>
-    </section>
+    <MapContainer
+      center={[27.7172, 85.3240]}   // Kathmandu
+      zoom={20}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="© OpenStreetMap contributors"
+      />
+    </MapContainer>
   );
 }
 
